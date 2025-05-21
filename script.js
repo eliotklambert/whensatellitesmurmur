37069cb7c0ed45a9dd4965bd2eb710bf722403b3
elambert1999 <eliotklambert@gmail.com>
2025-05-21 15:22:12 -0400

    neww

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ document.addEventListener("DOMContentLoaded", function () {
   fetch("nav.html")
     .then((response) => response.text())
     .then((data) => {
-      document.getElementById("nav-placeholder").innerHTML = data;
+      document.getElementById("nav.html").innerHTML = data;
     });
 });
